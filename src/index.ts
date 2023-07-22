394e34443c4b35e76ad835690682c20c73b5f188
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-07-22 11:53:39 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log('staring function');
+		console.log('almost done....');
+
+		if (request.url === '/favicon') {
+			return new Response(null, {
+				status: 301,
+				headers: {
+					location: 'https://soundcloudtomp3.co/wp-content/uploads/2018/09/favicon.png',
+				},
+			});
+		}
+		return new Response('Added favicon!');
+	},
+};
